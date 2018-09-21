590875dfdfe169aae1b43152896444ea9f310de4
cereceres <jesus.edelcereceres@gmail.com>
2018-09-20 22:38:26 -0600

    error to wrong type is added

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const get = require('./lib/get');
 
 const Mezquite = module.exports = function() {
-  if (!(this instanceof Mezquite)) return new Mezquite();
+    if (!(this instanceof Mezquite)) return new Mezquite();
 
-  this.methods = new Map();
-  this.setMethod = function(name, method) {
-    if (!name || !method || typeof method !== 'function') return this;
+    this.methods = new Map();
+    this.setMethod = function(name, method) {
+        if (!name || !method || typeof method !== 'function') return this;
 
-    this.methods.set(name, method);
+        this.methods.set(name, method);
 
-    return this;
-  };
+        return this;
+    };
 
-  this.get = get.bind(this);
+    this.get = get.bind(this);
 };
