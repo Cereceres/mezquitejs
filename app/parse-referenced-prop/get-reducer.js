a7785b9542d6cfa08bc71c8367ee14dc24fc88da
cereceres <jesus.edelcereceres@gmail.com>
2018-09-20 22:45:11 -0600

    syntaxis is fixed

--- app/parse-referenced-prop/get-reducer.js
+++ app/parse-referenced-prop/get-reducer.js
@@ -1,26 +1,26 @@
 
-module.exports = (params)=>{
-  const {parse, mezquite, refKey, refHereKey, object, source} = params;
-  return (previous, [key, value]) => {
-    if (!value) return previous;
+module.exports = (params) => {
+    const { parse, mezquite, refKey, refHereKey, object, source } = params;
+    return (previous, [ key, value ]) => {
+        if (!value) return previous;
 
-    const isRefKey = typeof value[refKey] !== 'undefined';
-    if (isRefKey) previous[key] = mezquite.get(source, value[refKey]);
+        const isRefKey = typeof value[refKey] !== 'undefined';
+        if (isRefKey) previous[key] = mezquite.get(source, value[refKey]);
 
-    const isRefHereKey = typeof value[refHereKey] !== 'undefined';
-    if (isRefHereKey) previous[key] = mezquite.get(object, value[refHereKey]);
+        const isRefHereKey = typeof value[refHereKey] !== 'undefined';
+        if (isRefHereKey) previous[key] = mezquite.get(object, value[refHereKey]);
 
-    if (!previous[key]) return previous;
+        if (!previous[key]) return previous;
 
-    if (typeof previous[key][refKey] !== 'undefined') previous[key] = mezquite.get(source, previous[key][refKey]);
+        if (typeof previous[key][refKey] !== 'undefined') previous[key] = mezquite.get(source, previous[key][refKey]);
 
-    if (typeof previous[key][refHereKey] !== 'undefined') previous[key] = mezquite.get(object, previous[key][refHereKey]);
+        if (typeof previous[key][refHereKey] !== 'undefined') previous[key] = mezquite.get(object, previous[key][refHereKey]);
 
-    const isPropParseable = typeof previous[key] === 'object' &&
+        const isPropParseable = typeof previous[key] === 'object' &&
         Object.keys(previous[key]).length;
 
-    if (isPropParseable) previous[key] = parse(previous[key], source);
+        if (isPropParseable) previous[key] = parse(previous[key], source);
 
-    return previous;
-  };
+        return previous;
+    };
 };
--- test/test.js
+++ test/test.js
@@ -1,116 +1,115 @@
-'use strict';
 const assert = require('assert');
 const Mezquite = require('../index');
 const mezquite = new Mezquite();
 describe('test to mezquitejs', () => {
-  it('should return a object', function() {
-    assert(typeof mezquite.get() === 'object');
-  });
-
-  it('should map a object with key given', function() {
-    let mapped = mezquite.get({'test2': 'test3'}, {'test1': 'test2'});
-    assert(mapped.test1 === 'test3');
-  });
-
-  it('should map and return undefined', function() {
-    let mapped = mezquite.get('', {'test1': 'test2.test3.test4'});
-    assert(mapped.test1 === undefined);
-  });
-  it('should do a loop over all keys given', function() {
-    let source = {
-      'test2': 'test3',
-      'key2': 'key3'
-    };
-    let map = {
-      'test1': 'test2',
-      'key1': 'key2'
-
-    };
-    let mapped = mezquite.get(source, map);
-    assert(mapped.test1 === 'test3');
-    assert(mapped.key1 === 'key3');
-  });
-
-  it('should go inside of a object', function() {
-    let source = {
-      'test2': {
-        test3: {test4: 'test5'}
-      }
-    };
-    let map = {
-      'test1': 'test2.test3.test4'
-    };
-    let mapped = mezquite.get(source, map);
-    assert.equal(mapped.test1, 'test5');
-  });
-
-  it('should stop of find a undefined value', function() {
-    let source = {
-      'test2': {
-        test3: undefined
-      }
-    };
-    let map = {
-      'test1': 'test2.test3.test4'
-    };
-    let mapped = mezquite.get(source, map);
-    assert(!mapped.test1);
-  });
-
-  it('should return the value correct with string', function() {
-    let source = {
-      'test2': {
-        test3: undefined
-      }
-    };
-    let map = 'test2.test3.test4';
-    let mapped = mezquite.get(source, map);
-    assert(mapped === undefined);
-  });
-
-  it('should exec the method given', function(done) {
-    let source = {
-      'test2': {
-        test3: {test4: 'test5'}
-      }
-    };
-    let map = {
-      'test1': 'test2.$method.test3.test4'
-    };
-    let called;
-    mezquite.setMethod('method', function(mapped) {
-      assert(mapped.test3.test4 === 'test5');
-      called = true;
-      return mapped;
+    it('should return a object', () => {
+        assert(typeof mezquite.get() === 'object');
     });
-    let mapped = mezquite.get(source, map);
-    assert(mapped.test1 === 'test5');
-    if (called) done();
-  });
 
-  it('should try to go inside if method is not defined', function() {
-    let source = {
-      'test2': {
-        test3: {test4: 'test5'}
-      }
-    };
-    let map = {
-      'test1': 'test2.$test3.test4'
-    };
-    let mapped = mezquite.get(source, map);
-    assert(mapped.test1 === 'test5');
-  });
+    it('should map a object with key given', () => {
+        const mapped = mezquite.get({ test2: 'test3' }, { test1: 'test2' });
+        assert(mapped.test1 === 'test3');
+    });
 
-  it('should return undefined if method given is not defined', function() {
-    let source = {
-      'test2': {
-        test3: {test4: 'test5'}
-      }
-    };
-    let map = {
-      'test1': 'test2.$fake.test3.test4'
-    };
-    let mapped = mezquite.get(source, map);
-    assert(!mapped.test1);
-  });
+    it('should map and return undefined', () => {
+        const mapped = mezquite.get('', { test1: 'test2.test3.test4' });
+        assert(mapped.test1 === undefined);
+    });
+    it('should do a loop over all keys given', () => {
+        const source = {
+            test2: 'test3',
+            key2: 'key3'
+        };
+        const map = {
+            test1: 'test2',
+            key1: 'key2'
+
+        };
+        const mapped = mezquite.get(source, map);
+        assert(mapped.test1 === 'test3');
+        assert(mapped.key1 === 'key3');
+    });
+
+    it('should go inside of a object', () => {
+        const source = {
+            test2: {
+                test3: { test4: 'test5' }
+            }
+        };
+        const map = {
+            test1: 'test2.test3.test4'
+        };
+        const mapped = mezquite.get(source, map);
+        assert.equal(mapped.test1, 'test5');
+    });
+
+    it('should stop of find a undefined value', () => {
+        const source = {
+            test2: {
+                test3: undefined
+            }
+        };
+        const map = {
+            test1: 'test2.test3.test4'
+        };
+        const mapped = mezquite.get(source, map);
+        assert(!mapped.test1);
+    });
+
+    it('should return the value correct with string', () => {
+        const source = {
+            test2: {
+                test3: undefined
+            }
+        };
+        const map = 'test2.test3.test4';
+        const mapped = mezquite.get(source, map);
+        assert(mapped === undefined);
+    });
+
+    it('should exec the method given', (done) => {
+        const source = {
+            test2: {
+                test3: { test4: 'test5' }
+            }
+        };
+        const map = {
+            test1: 'test2.$method.test3.test4'
+        };
+        let called;
+        mezquite.setMethod('method', (mapped) => {
+            assert(mapped.test3.test4 === 'test5');
+            called = true;
+            return mapped;
+        });
+        const mapped = mezquite.get(source, map);
+        assert(mapped.test1 === 'test5');
+        if (called) done();
+    });
+
+    it('should try to go inside if method is not defined', () => {
+        const source = {
+            test2: {
+                test3: { test4: 'test5' }
+            }
+        };
+        const map = {
+            test1: 'test2.$test3.test4'
+        };
+        const mapped = mezquite.get(source, map);
+        assert(mapped.test1 === 'test5');
+    });
+
+    it('should return undefined if method given is not defined', () => {
+        const source = {
+            test2: {
+                test3: { test4: 'test5' }
+            }
+        };
+        const map = {
+            test1: 'test2.$fake.test3.test4'
+        };
+        const mapped = mezquite.get(source, map);
+        assert(!mapped.test1);
+    });
 });
